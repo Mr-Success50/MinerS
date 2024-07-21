09efa5bcc0fa6144449e0cf7a88da2a65ba40884
success <instantmoneyforum@gmail.com>
2024-07-21 03:02:29 +0100

    initial-commit

---
+++ src/counter.ts
@@ -0,0 +1,31 @@
+export function setupCounter(element: HTMLButtonElement) {
+  let counter = 0
+  const setCounter = (count: number) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
+
+export function handleClick(): void {
+  const element = document.getElementById('udc_wrap');
+  const btn_element = document.getElementById('btn');
+  const btn_element2 = document.getElementById('btn2');
+  if (element) {
+    element.style.display = "block";
+    btn_element.style.display = "none";
+    btn_element2.style.display = "block";
+  }
+}
+
+export function handleClick2(): void { 
+  const element = document.getElementById('udc_wrap');
+  const btn_element = document.getElementById('btn');
+  const btn_element2 = document.getElementById('btn2');
+  if (element) {
+    element.style.display = "none";
+    btn_element.style.display = "block";
+    btn_element2.style.display = "none";
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import './styles/style.css' //For importing links brandLogo
+import brandLogo from '/images/vite.svg'
+import { handleClick } from './counter.ts' //importing a function from and API endpoint
+import { handleClick2 } from './counter.ts' //importing a function from and API endpoint
+//________________________________________________________________________
+import { retrieveLaunchParams } from '@telegram-apps/sdk'; 
+  
+//________________________________________________________________________
+var logoImage = document.getElementById("logo") as HTMLImageElement;
+logoImage.src = `${brandLogo}`;
+
+//____Telegram User Data__________________________________________________
+const contentData0 = document.getElementById("userDataContent");
+
+if (contentData0) {
+  //const initDataRaw = "Your content goes here"; // Replace this with your actual data
+  const { initDataRaw } = retrieveLaunchParams();
+  contentData0.textContent = initDataRaw;
+} else {
+  console.error("Element with ID 'userDataContent' not found.");
+}
+//___CLOSED____Telegram User Data___________________________________________
+
+
+
+// for launch btn
+const button1 = document.getElementById('btn');
+if (button1) {
+  button1.addEventListener('click', handleClick);
+}
+
+// for close launch btn
+const button2 = document.getElementById('btn2');
+if (button2) {
+  button2.addEventListener('click', handleClick2);
+}
